f7bfb66e9077f4f7ba91441b8d91a9fd6847440c
tanimtorikul <torikulswe@gmail.com>
2024-10-13 16:10:03 +0600

    implemented category page for products to show when specific category is clicked

--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -1,11 +1,47 @@
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useProducts from "../../hooks/useProducts";
+import ProductCard from "../../components/shared/ProductCard";
 
 const CategoryPage = () => {
-    return (
-        <div>
-            
-        </div>
+  const { categoryName } = useParams();
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [products] = useProducts();
+
+  useEffect(() => {
+    const filtered = products.filter((product) =>
+      product.category.includes(categoryName.toLowerCase())
     );
+    setFilteredProducts(filtered);
+  }, [categoryName, products]);
+
+  //   console.log(filteredProducts);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div>
+      <div className="max-w-[1400px] mx-auto bg-teal-500 my-2 py-24 text-white">
+        <h2 className="capitalize text-2xl font-bold text-center">
+          {categoryName}
+        </h2>
+        <p className="my-4 text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          officia sapiente nostrum magni reprehenderit, modi eius consequuntur
+          neque aperiam eos!
+        </p>
+      </div>
+
+      {/* Product Cards */}
+      <div className="max-w-[1400px] mx-auto grid grid-cols-2 md:grid-cols-5 gap-4">
+        {filteredProducts?.map((product) => (
+          <ProductCard key={product.id} item={product} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryPage;
