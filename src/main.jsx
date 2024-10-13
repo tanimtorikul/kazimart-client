256f4ccc96f2b9681da685e38cf352f563ff599e
tanimtorikul <torikulswe@gmail.com>
2024-10-13 13:09:34 +0600

    implemented context api authprovider and a custom hook useAuth

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes'
+import AuthProvider from './Providers/AuthProviders/AuthProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+   <AuthProvider>
+   <RouterProvider router={router} />
+   </AuthProvider>
   </StrictMode>,
 )
