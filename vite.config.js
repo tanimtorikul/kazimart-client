91485a5e8d85b4c0a23d7929c6da3b3178a67039
tanimtorikul <torikulswe@gmail.com>
2024-10-16 17:20:57 +0600

    deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory
+    rollupOptions: {
+      // Additional Rollup options can be configured here
+      input: {
+        main: './index.html', // Ensure it points to your main HTML file
+      },
+    },
+  },
+});
