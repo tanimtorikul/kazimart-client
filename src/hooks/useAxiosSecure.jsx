9ee8795183899169206dbad7c657395ffc901166
tanimtorikul <torikulswe@gmail.com>
2024-10-16 14:44:19 +0600

    implemented logout for unauthorized access

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,15 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 // Create a new Axios instance
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000/", // Make sure this is the correct base URL
+  baseURL: "http://localhost:5000/",
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
   // request interceptor to add authorization header for every secure call to api
   axiosSecure.interceptors.request.use(
     function (config) {
@@ -15,6 +19,22 @@ const useAxiosSecure = () => {
       return config;
     },
     function (error) {
+      return Promise.reject(error);
+    }
+  );
+  // intercepts 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      console.log("status error", status);
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+
       return Promise.reject(error);
     }
   );
