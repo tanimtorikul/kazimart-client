b33d63f968215cd97da67648cfbd1cc6eaf656d5
tanimtorikul <torikulswe@gmail.com>
2024-10-18 14:58:22 +0600

    implemented handledelete to delete category and used refetch

--- src/components/Categories/CategoriesList.jsx
+++ src/components/Categories/CategoriesList.jsx
@@ -1,8 +1,22 @@
+import { MdEdit } from "react-icons/md";
 import useCategories from "../../hooks/useCategories";
+import { FaTrashAlt } from "react-icons/fa";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const CategoriesList = () => {
-  const { categories } = useCategories(); 
- 
+  const { categories, refetch } = useCategories();
+  const axiosSecure = useAxiosSecure();
+
+  //   handle delete to delete category
+  const handleDelete = (id, category) => {
+    axiosSecure.delete(`/categories/${id}`).then((res) => {
+      if (res.data.deletedCount > 0) {
+        refetch();
+        toast.success(`${category} is deleted from the category list`);
+      }
+    });
+  };
 
   return (
     <div className="overflow-x-auto py-4">
@@ -29,20 +43,20 @@ const CategoriesList = () => {
               <td className="py-3 px-5 text-center">{index + 1}</td>
               <td className="py-3 px-5 flex justify-center">
                 <img
-                  src={category.imgUrl} 
+                  src={category.imgUrl}
                   className="w-16 h-8 md:w-32 md:h-16 object-cover"
                 />
               </td>
               <td className="py-2 px-2 text-center">{category.category}</td>
-              <td className="py-3 px-5 hidden sm:table-cell text-center">
+              <td className="py-3 px-5 text-center">
                 <button className="border-blue-500 text-blue-500 border px-1 py-1 rounded">
-                 
+                  <MdEdit />
                 </button>
                 <button
-                
+                  onClick={() => handleDelete(category._id, category.category)}
                   className="border-red-500 text-red-500 border px-1 py-1 rounded md:ml-3"
                 >
-              
+                  <FaTrashAlt />
                 </button>
               </td>
             </tr>
