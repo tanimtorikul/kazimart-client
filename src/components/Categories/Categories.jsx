5b417febae606daa17a60f8d2519881721743610
tanimtorikul <torikulswe@gmail.com>
2024-10-18 13:17:10 +0600

    fetched categories dynamically from api

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,25 +1,10 @@
-import { useEffect, useState } from "react";
 import SectionTitle from "../shared/SectionTitle";
 import { Link } from "react-router-dom";
 import CategoryCard from "./CategoryCard";
+import useCategories from "../../hooks/useCategories";
 
 const Categories = () => {
-  const [categories, setCategories] = useState([]);
-
-  // fetching categories data
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const response = await fetch("categories.json");
-        const data = await response.json();
-        setCategories(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchCategories();
-  }, []);
+  const { categories } = useCategories(); 
 
   return (
     <div className="my-16">
@@ -29,8 +14,8 @@ const Categories = () => {
       />
 
       <div className="max-w-[1400px] mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6 mt-6">
-        {categories.map((category) => (
-          <Link key={category.id} to={`/categories/${category.category}`}>
+        {categories.map((category) => ( 
+          <Link key={category._id} to={`/categories/${category.name}`}>
             <CategoryCard category={category} />
           </Link>
         ))}
