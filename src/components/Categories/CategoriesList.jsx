582db343780fc4512e8795738ccbc632334723ba
tanimtorikul <torikulswe@gmail.com>
2024-10-18 13:20:56 +0600

    displayed categories in add category page category list

--- src/components/Categories/CategoriesList.jsx
+++ src/components/Categories/CategoriesList.jsx
@@ -1,4 +1,9 @@
+import useCategories from "../../hooks/useCategories";
+
 const CategoriesList = () => {
+  const { categories } = useCategories(); 
+ 
+
   return (
     <div className="overflow-x-auto py-4">
       <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-sm">
@@ -18,7 +23,31 @@ const CategoriesList = () => {
             </th>
           </tr>
         </thead>
-        <tbody className="bg-white divide-y divide-gray-200"></tbody>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {categories.map((category, index) => (
+            <tr key={category._id}>
+              <td className="py-3 px-5 text-center">{index + 1}</td>
+              <td className="py-3 px-5 flex justify-center">
+                <img
+                  src={category.imgUrl} 
+                  className="w-16 h-8 md:w-32 md:h-16 object-cover"
+                />
+              </td>
+              <td className="py-2 px-2 text-center">{category.category}</td>
+              <td className="py-3 px-5 hidden sm:table-cell text-center">
+                <button className="border-blue-500 text-blue-500 border px-1 py-1 rounded">
+                 
+                </button>
+                <button
+                
+                  className="border-red-500 text-red-500 border px-1 py-1 rounded md:ml-3"
+                >
+              
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
